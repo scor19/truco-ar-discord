2bdced0898e2c181df32a21eff279f34050109df
scor19 <roccobazo@gmail.com>
2024-04-09 17:18:42 -0300

    Replaced HTTP Requests with WebSockets (Socket.IO)

--- server/src/server.js
+++ server/src/server.js
@@ -1,20 +1,74 @@
 import express from "express";
 import dotenv from "dotenv";
-import apiRoutes from "./routes/apiRoutes.js";
-import gameRoutes from "./routes/gameRoutes.js";
-import lobbyRoutes from "./routes/lobbyRoutes.js";
+import {
+  getGames,
+  joinGame,
+  leaveGame,
+  createGame,
+} from "./game/data/lobbiesLogic.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { clientMessages, serverMessages } from "./websocketEvents.js";
 
 dotenv.config({ path: "../.env" });
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 const port = 3001;
+
+// Websocket
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on(clientMessages.GET_GAMES, async () => {
+    try {
+      const gamesData = await getGames();
+      socket.emit(serverMessages.GAMES_DATA, gamesData);
+    } catch (error) {
+      console.error("Error al manejar mensaje del cliente:", error);
+      // Enviar un mensaje de error al cliente si es necesario
+    }
+  });
+
+  socket.on(clientMessages.CREATE_GAME, async (data) => {
+    try {
+      const game = createGame(data.player_host, data.max_players);
+      socket.emit(serverMessages.GAME_CREATED, game);
+    } catch (error) {
+      console.error("Error al manejar mensaje del cliente:", error);
+      // Enviar un mensaje de error al cliente si es necesario
+    }
+  });
+
+  socket.on(clientMessages.JOIN_GAME, async (data) => {
+    try {
+      const joinedGame = await joinGame(data.gameId, data.player);
+      socket.emit(serverMessages.GAME_JOINED, joinedGame);
+    } catch (error) {
+      console.error("Error al manejar mensaje del cliente:", error);
+      // Enviar un mensaje de error al cliente si es necesario
+    }
+  });
+
+  socket.on(clientMessages.LEAVE_GAME, async (data) => {
+    try {
+      const leftGame = await leaveGame(data.gameId, data.player);
+      socket.emit(serverMessages.GAME_LEFT, leftGame);
+    } catch (error) {
+      console.error("Error al manejar mensaje del cliente:", error);
+      // Enviar un mensaje de error al cliente si es necesario
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
 
 // Allow express to parse JSON bodies
 app.use(express.json());
-app.use("/api", apiRoutes);
-app.use("/game", gameRoutes);
-app.use("/lobby", lobbyRoutes);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
---
+++ server/src/websocketEvents.js
@@ -0,0 +1,15 @@
+// Mensajes enviados desde el cliente al servidor
+export const clientMessages = {
+  GET_GAMES: "get_games",
+  CREATE_GAME: "create_game",
+  JOIN_GAME: "join_game",
+  LEAVE_GAME: "leave_game",
+};
+
+// Mensajes enviados desde el servidor al cliente
+export const serverMessages = {
+  GAMES_DATA: "games_data",
+  GAME_CREATED: "game_created",
+  GAME_JOINED: "game_joined",
+  GAME_LEFT: "game_left",
+};
